afbe067048c822fbe75378914db3df88d8f96889
Foster06 <fortesondorcius03@gmail.com>
2017-07-20 18:53:44 -0400

    First commit

---
+++ app/src/main/java/com/rodex/gitdemo/GitActivity.java
@@ -0,0 +1,13 @@
+package com.rodex.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+}
